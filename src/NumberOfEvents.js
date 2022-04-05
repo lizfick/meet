e77cfa58be83c729ea31d7f479f9da81a6684458
lizfick <fickerliz@gmail.com>
2022-04-04 21:10:43 -0400

    unit testing

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32,
+    infoText: "",
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if (value < 1 || value > 32) {
+      this.setState({
+        numberOfEvents: "",
+        infoText: "Please enter a number between 1 and 32",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        infoText: "",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="NumberOfEvents">
+        <br></br>
+        <p>Number of Events:</p>
+        <input
+          type="number"
+          className="numberOfEvents"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        />
+      </div>
+    );
+  }
+}
+export default NumberOfEvents;
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+import { shallow } from 'enzyme';
+
+describe('<Event /> component', () => {
+
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[1]} />);
+  });
+
+  test('render an event', () => {
+    expect(EventWrapper.find('.event')).toHaveLength(1);
+  });
+
+  test('render a location', () => {
+    expect(EventWrapper.find('.location')).toHaveLength(1);
+  });
+
+  test('render start date', () => {
+    expect(EventWrapper.find('.start-date')).toHaveLength(1);
+  });
+
+  test('event details are collapsed by default', () => {
+    expect(EventWrapper.state('collapsed')).toBe(true);
+  });
+
+  test('render details button', () => {
+    expect(EventWrapper.find('.details-button')).toHaveLength(1);
+  });
+
+  test('render details when show details button is clicked', () => {
+    EventWrapper.setState({ collapsed: true });
+    EventWrapper.find('.details-button').simulate('click');
+    expect(EventWrapper.state('collapsed')).toBe(false);
+  });
+
+  test('hide details when button is clicked again', () => {
+    EventWrapper.setState({ collapsed: false });
+    EventWrapper.find('.hide-details').simulate('click');
+    expect(EventWrapper.state('collapsed')).toBe(true);
+  });
+})
